b32f9f232212fc95c7b3399bc954ad5ea1ec21b4
Jose Morales <jmoralesmnz@gmail.com>
2018-12-31 10:45:45 +0100

    Create SQL.mk

---
+++ SQL.mk
@@ -0,0 +1,13 @@
+**Redundancia **: Eliminación de Renundancia de Datos
+Concurrencia : Posibilidad de accedes a datos de manera concurrente.
+Aislamiento: Permite realizar operaciones independientes
+Integridad: PK, not null, true , false o FK
+Inconsistencia: Permite evitar la insconsistencia de datos (Bogotá con tilde en un registro, Bogota sin tilde en otro registro) refiriendose al mismo dato
+Seguridad: Autenticación, LDAP, Kerberos
+Acceso : Permite manejas esquemas de seguridad, ROLES ó noveles de accesos
+Atomicidad :
+
+DML: DATA MODEL LANGUAGE
+DDL: DATA DEFINITION LANGUAGE
+
+Mysql fue adquirido por Oracle. Maria db es una especie de fork de mysql pero cuenta con una buena comunidad.
